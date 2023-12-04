1d2730f3547ab4d17a8e006abc5e9984a6eb66fc
Ferran <ferran.docencia@gmail.com>
2023-12-04 10:52:54 +0100

    Primer commit con las variables de Python

---
+++ 01_variables.py
@@ -0,0 +1,10 @@
+# Crear una variable en Python
+nombre = "Anna" # tipo String
+numero1 = 10
+numero2 = 10.5
+booleano1 = True
+booleano2 = False
+
+# print es una función que muestra información por el terminal
+print(nombre)
+print(type(numero1))
