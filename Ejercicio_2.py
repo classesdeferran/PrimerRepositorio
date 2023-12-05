efc4ab2dea0ff5b08effb4ae5dff4cb50ef809cf
Ferran <ferran.docencia@gmail.com>
2023-12-05 13:10:46 +0100

    Añadidos ejercicios

---
+++ Ejercicio_2.py
@@ -0,0 +1,8 @@
+# Vamos a un comercio y compramos algo
+# El usuario nos indica el precio y lo que paga
+
+# Si paga menos de lo que vale indicamos que falta dinero
+
+# Si paga correctamente devolvemos el cambio de tal manera 
+# que lo haremos con los billetes y monedas de mayor valor posible
+
